808c876050e075b6386a0395bcc0045969ed2f7a
eric ghost <shdong@hotmail.com>
2019-06-17 01:32:27 +0900

    test/wifiap_evaluate2_test.sh is written.

--- test/wifiap_evaluate2_test.sh
+++ test/wifiap_evaluate2_test.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
-# wifiap_evaluate2.sh force_scan interface
+
 # wifiap_evaluate2.sh scan interface ssid
 # wifiap_evaluate2.sh deassociate interface
 # wifiap_evaluate2.sh associate interface ssid passwd
 # wifiap_evaluate2.sh connect interface ip_address
 # wifiap_evaluate2.sh connect_internet interface
-# wifiap_evaluate2.sh regulatory_domain country_code
+# wifiap_evaluate2.sh regulatory_domain interface country_code
 # wifiap_evaluate2.sh refresh_netif interface
 
-while :
-do
+while :; do
+
   wifiap_evaluate2.sh refresh_netif wlan0
-  ip link set wlan0 down
   sleep 1
-  wifiap_evaluate2.sh regulatory_domain wlan0 KR
+  if [ $? -ne 0 ]; then
+    continue
+  fi
+
+  wifiap_evaluate2.sh scan wlan0 coffeebreak_5g
   sleep 1
-  modprobe -r brcmfmac
-  sleep 1
-  modprobe brcmfmac
-  sleep 1
-  ip link set wlan0 up 
-  sleep 1
-  wifiap_evaluate2.sh deassociate wlan0
+  if [ $? -ne 0 ]; then
+    continue
+  fi
+
   wifiap_evaluate2.sh associate wlan0 coffeebreak_5g coffeebreak1** 
-  wifiap_evaluate2.sh connect wlan0 192.168.35.1
+  sleep 10 
+  if [ $? -ne 0 ]; then
+    continue
+  fi
+
+  gateway=`netstat -rn | grep UG | grep $2 | awk '{print $2}'`
+  wifiap_evaluate2.sh connect wlan0 $gateway 
+  if [ $? -ne 0 ]; then
+    continue
+  fi
+
   wifiap_evaluate2.sh connect_internet wlan0
+  if [ $? -ne 0 ]; then
+    continue
+  fi
+
 done
 
 # eof
