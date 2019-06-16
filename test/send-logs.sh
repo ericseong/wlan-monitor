05232e8d02853e16134836238c4bea6ac01b0eda
eric ghost <shdong@hotmail.com>
2019-06-17 01:46:29 +0900

    Code beautification for test/send-logs.sh

--- test/send-logs.sh
+++ test/send-logs.sh
@@ -6,20 +6,13 @@ function send_logs()
   now=$(date +'%Y%m%d-%H%M')
   dmesg > "${now}-dmesg.txt"
 
-  # get wlanmonitor service log
   systemctl -n 100 status wlanmonitor > "${now}-systemctl_status.txt"
-
-  # get syslog
   tail -n 500 /var/log/syslog > "${now}-syslog.txt"
-
-  # get mail log
   tail -n 100 /var/log/mail.log > "${now}-mail-log.txt"
   tail -n 100 /var/log/mail.err > "${now}-mail-err.txt"
 
-  # now, send those files to mail
-  # It seems that there's no way we can send body + attachment with mail utility \
-  # and thus only attachments are sent without message body.
-echo '' |  mail -s "Message from wlanmonitor_controller. This message has been sent because wlanmonitor_controller can't recover wlanmonitor to normal state and thus decide to reboot. Before issuing reboot, this message has been sent." \
+  # Now, let's send those files to mail. It seems that there's no way we can send body + attachment with mail utility and thus only attachments are sent without message body.
+  echo '' |  mail -s "Sent from send_logs()" \
   -A "${now}-dmesg.txt" \
   -A "${now}-systemctl_status.txt" \
   -A "${now}-syslog.txt" \
